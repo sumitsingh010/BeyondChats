e7a60988be8c57f1e74147a850eec6ae5c299cfa
sumitsingh010 <sengarsumit7047@gmail.com>
2025-12-28 22:36:07 +0530

    Clean up repository - remove unnecessary files

--- start-backend.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting Backend Server...
-echo.
-cd backend
-echo Backend running on http://localhost:5000
-echo Press Ctrl+C to stop
-echo.
-npm start
--- start-frontend.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting Frontend...
-echo.
-cd frontend
-echo Frontend will open at http://localhost:3000
-echo Press Ctrl+C to stop
-echo.
-npm start
